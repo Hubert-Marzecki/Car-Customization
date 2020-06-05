d281656bb695fed76c20a067699cfd54bbcf33d2
Hubert Marzecki <hubert.marzecki@gmail.com>
2020-06-05 15:19:32 +0200

    added Pagination

---
+++ src/components/buttons/PaginationButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./_pagination__button.scss"
+
+export function PaginationButton(props:{
+    text:string,
+    className:string,
+    newOffset: (offset:number, target:string, limit:number) => void
+    offset: number,
+    limit:number,
+    target: string
+}) {
+    return (
+        <button className={props.className} onClick={() => props.newOffset(props.offset, props.target, props.limit)}>
+            {props.text}
+        </button>
+    )
+}
